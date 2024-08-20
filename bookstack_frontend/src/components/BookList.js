57d9e1d32fa215fb7357330bc6002136f1724959
isheikh8492 <60596395+isheikh8492@users.noreply.github.com>
2024-08-20 00:01:43 -0500

    frontend improvement

--- bookstack_frontend/src/components/BookList.js
+++ bookstack_frontend/src/components/BookList.js
@@ -21,15 +21,23 @@ function BookList() {
       status: "Reading",
     },
   ]);
+
   const [newBook, setNewBook] = useState({
     title: "",
     author: "",
     priority: "Low",
     status: "In Queue",
   });
+
+  const [filter, setFilter] = useState({
+    title: "",
+    author: "",
+    priority: "",
+  });
+
   const [activeTab, setActiveTab] = useState("toRead");
-  const [sortField, setSortField] = useState("title");
-  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortField, setSortField] = useState("title"); // Define sortField
+  const [sortOrder, setSortOrder] = useState("asc"); // Define sortOrder
 
   const addBook = () => {
     setBooks([...books, { id: Date.now(), ...newBook, isRead: false }]);
@@ -48,11 +56,18 @@ function BookList() {
     );
   };
 
+  const handleFilterChange = (e) => {
+    const { name, value } = e.target;
+    setFilter({
+      ...filter,
+      [name]: value,
+    });
+  };
+
   const sortBooks = (books) => {
     return books.sort((a, b) => {
       const aValue = a[sortField].toLowerCase();
       const bValue = b[sortField].toLowerCase();
-
       if (sortOrder === "asc") {
         return aValue > bValue ? 1 : -1;
       } else {
@@ -61,10 +76,18 @@ function BookList() {
     });
   };
 
-  const filteredBooks =
-    activeTab === "toRead"
-      ? books.filter((book) => book.status !== "Completed")
-      : books.filter((book) => book.status === "Completed");
+  const filteredBooks = books.filter((book) => {
+    return (
+      (filter.title === "" ||
+        book.title.toLowerCase().includes(filter.title.toLowerCase())) &&
+      (filter.author === "" ||
+        book.author.toLowerCase().includes(filter.author.toLowerCase())) &&
+      (filter.priority === "" || book.priority === filter.priority) &&
+      (activeTab === "toRead"
+        ? book.status !== "Completed"
+        : book.status === "Completed")
+    );
+  });
 
   return (
     <div className="container mx-auto p-4">
@@ -92,48 +115,46 @@ function BookList() {
         </button>
       </div>
 
-      {/* Input for adding a new book */}
-      {activeTab === "toRead" && (
-        <div className="mb-4">
-          <div className="flex space-x-2 mb-4">
-            <input
-              type="text"
-              placeholder="Title"
-              value={newBook.title}
-              onChange={(e) =>
-                setNewBook({ ...newBook, title: e.target.value })
-              }
-              className="p-2 border border-gray-300 rounded-md"
-            />
-            <input
-              type="text"
-              placeholder="Author"
-              value={newBook.author}
-              onChange={(e) =>
-                setNewBook({ ...newBook, author: e.target.value })
-              }
-              className="p-2 border border-gray-300 rounded-md"
-            />
-            <select
-              value={newBook.priority}
-              onChange={(e) =>
-                setNewBook({ ...newBook, priority: e.target.value })
-              }
-              className="p-2 border border-gray-300 rounded-md"
-            >
-              <option value="Low">Low</option>
-              <option value="Medium">Medium</option>
-              <option value="High">High</option>
-            </select>
-            <button
-              onClick={addBook}
-              className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 w-10 h-10 flex items-center justify-center"
-            >
-              <FaPlus />
-            </button>
-          </div>
-        </div>
-      )}
+      {/* Filters */}
+      <div className="flex mb-4 space-x-4 items-center">
+        <input
+          type="text"
+          placeholder="Title"
+          name="title"
+          value={filter.title}
+          onChange={handleFilterChange}
+          className="p-2 border border-gray-300 rounded-md h-12 flex-grow"
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          name="author"
+          value={filter.author}
+          onChange={handleFilterChange}
+          className="p-2 border border-gray-300 rounded-md h-12 flex-grow"
+        />
+        <select
+          name="priority"
+          value={filter.priority}
+          onChange={handleFilterChange}
+          className="p-2 border border-gray-300 rounded-md h-12 text-gray-500"
+        >
+          <option value="" className="text-gray-400">
+            Priority
+          </option>
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+        </select>
+
+        {/* Add New Book Button */}
+        <button
+          onClick={addBook}
+          className="bg-blue-500 text-white rounded-md hover:bg-blue-600 w-12 h-12 flex items-center justify-center"
+        >
+          <FaPlus />
+        </button>
+      </div>
 
       {/* Table to display books */}
       <BookTable
